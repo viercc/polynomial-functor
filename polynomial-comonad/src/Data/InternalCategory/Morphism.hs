7de527b99534da1bd59bd16114540edbfe946315
Koji Miyazato <viercc@gmail.com>
2025-09-22 19:35:39 +0900

    Quiver morphism (experiment)

--- polynomial-comonad/src/Data/InternalCategory/Morphism.hs
+++ polynomial-comonad/src/Data/InternalCategory/Morphism.hs
@@ -2,39 +2,41 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE StandaloneDeriving #-}
 module Data.InternalCategory.Morphism(
-  Mor(..), identityMorphism, composeMorphism
+  Mor(..),
 ) where
 
 import Prelude hiding ((.), id)
-import qualified Data.List
-import Data.Maybe (fromMaybe)
-import Control.Category
+import Control.Category ( Category(..) )
 
 import Data.Singletons
 import Data.Singletons.Decide
 import Data.InternalCategory
-    ( IQuiver(..), ICategory(..), Path(Path) )
+    ( IQuiver(..), ICategory(..) )
 import Data.Kind (Type)
+import Data.Singletons.ShowSing ( ShowSing )
 
 -- | Sum of all morphisms in @cat@.
 data Mor k (cat :: k -> k -> Type) where
   Morphism :: Sing a -> Sing b -> cat a b -> Mor k cat
 
-instance IQuiver (SomeSing k) (Mor k cat) where
-  src (Morphism sa _ _) = SomeSing sa
-  tgt (Morphism _ sb _) = SomeSing sb
+-- | Objects in @cat@.
+data Ob k where
+  Ob :: !(Sing (a :: k)) -> Ob k
 
-identityMorphism :: (Category cat) => SomeSing k -> Mor k cat
-identityMorphism (SomeSing sa) = Morphism sa sa id
+deriving instance ShowSing k => Show (Ob k)
+instance SDecide k => Eq (Ob k) where
+  Ob x == Ob y = case x %~ y of
+    Proved _ -> True
+    Disproved _ -> False
 
-composeMorphism :: (Category cat, SDecide k) => Mor k cat -> Mor k cat -> Maybe (Mor k cat)
-composeMorphism (Morphism sa sb f) (Morphism sb' sc g) = case sb %~ sb' of
-  Proved Refl -> Just $ Morphism sa sc (g . f)
-  Disproved _ -> Nothing
+instance IQuiver (Ob k) (Mor k cat) where
+  src (Morphism sa _ _) = Ob sa
+  tgt (Morphism _ sb _) = Ob sb
 
-composeMorphism' :: (Category cat, SDecide k) => Mor k cat -> Mor k cat -> Mor k cat
-composeMorphism' f g = fromMaybe (error "Invalid Path") $ composeMorphism f g
-
-instance (Category cat, SDecide k) => ICategory (SomeSing k) (Mor k cat) where
-  foldPath (Path s0 mors _) = Data.List.foldl' composeMorphism' (identityMorphism s0) mors 
+instance (Category cat, SDecide k) => ICategory (Ob k) (Mor k cat) where
+  identity (Ob sa) = Morphism sa sa id
+  compose (Morphism sa sb f) (Morphism sb' sc g) = case sb %~ sb' of
+    Proved Refl -> Just $ Morphism sa sc (g . f)
+    Disproved _ -> Nothing
